cf0f3a6969828bf8456fc65dc7c1b3a3bf17bcb6
Giuseppe Rosa <giusepper11@gmail.com>
2020-06-24 16:05:14 -0300

    18638 - Corrigindo regra de BERCARIO I

--- cadastro_infantil/utils/define_agrupamento.py
+++ cadastro_infantil/utils/define_agrupamento.py
@@ -4,7 +4,8 @@ from datetime import date, datetime
 def define_agrupamento(dt_nascimento):
     ano_atual = datetime.now().year
     faixas = {
-        'BERCARIO I': date(ano_atual - 1, 4, 1) <= dt_nascimento <= date(ano_atual, 3, 31),
+        # 'BERCARIO I': date(ano_atual - 1, 4, 1) <= dt_nascimento <= date(ano_atual, 3, 31),
+        'BERCARIO I': date(ano_atual - 1, 4, 1) <= dt_nascimento <= datetime.now().date(),
         'BERCARIO II': date(ano_atual - 2, 4, 1) <= dt_nascimento <= date(ano_atual - 1, 3, 31),
         'MINI GRUPO I': date(ano_atual - 3, 4, 1) <= dt_nascimento <= date(ano_atual - 2, 3, 31),
         'MINI GRUPO II': date(ano_atual - 4, 4, 1) <= dt_nascimento <= date(ano_atual - 3, 3, 31),
